aba828e2bcc34b49cfa78981281c2d984dc5636b
Luke859 <71589052+Luke859@users.noreply.github.com>
2022-03-02 22:57:15 +0100

    Style - ajout vague et informations de contact et le style de la page contact

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,7 @@
 <template>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style="vertical-align: top">
+  <path fill="#42a5f5" fill-opacity="1" d="M0,128L60,112C120,96,240,64,360,53.3C480,43,600,53,720,96C840,139,960,213,1080,229.3C1200,245,1320,203,1380,181.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+</svg>
     <section class="s2">
         <div class="main-container">
             <div class="greeting">
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,41 +1,82 @@
 <template>
     <section class="s5">
         <div class="main-container">
-            <h1>CONTACT</h1>
-            <form id="contact-form" action="https://formsubmit.co/lukebryan@orange.fr" method="POST"  enctype="multipart/form-data" >
-                <input type="hidden" name="_captcha" value="false">
-                <input class="input-field" type="text" name="Prénom" placeholder="Prénom *" required>
-                <input class="input-field" type="text" name="Nom" placeholder="Nom *" required>
-                <input class="input-field" type="text" name="Email" placeholder="Email *" required>
-                <input type="hidden" name="_next" value="http://localhost:3000/">
-                <textarea class="input-field" name="Message" required></textarea>
-                <input type="file" name="attachment" accept="image/png, image/jpeg">
-                <input id="submit-btn" type="submit" value="Envoyer">
-            </form>
+            <h1>ME CONTACTER</h1>
+            <div class="contact">
+                <form id="contact-form" action="https://formsubmit.co/lukebryan@orange.fr" method="POST"  enctype="multipart/form-data" >
+                    <input type="hidden" name="_captcha" value="false">
+                    <input class="input-field" type="text" name="Prénom" placeholder="Prénom *" required>
+                    <input class="input-field" type="text" name="Nom" placeholder="Nom *" required>
+                    <input class="input-field" type="text" name="Email" placeholder="Email *" required>
+                    <input type="hidden" name="_next" value="http://localhost:3000/">
+                    <textarea class="input-field" name="Message" required></textarea>
+                    <!-- <input type="file" name="attachment" accept="image/png, image/jpeg"> -->
+                    <input id="submit-btn" type="submit" value="Envoyer">
+                </form>
+
+                <div class="contact-text">
+                    <h3>Informations</h3>
+                    <p>Si vous avez plus de questions n'hésiter pas à me contacter</p>
+                    <div class="info">
+                        <li><a class="fa fa-phone" ></a> +33 671771971</li>
+                        <li><a class="fa fa-envelope" ></a> lukebryan@orange.fr</li>
+                        <li><a class="fa fa-map-marker" ></a> 13 Rue Stéphane LeDuc, Nantes 44100</li>
+                    </div>
+                    <div class="reseaux">
+                        <li><a target="_blank" class="fa fa-facebook" href="https://www.facebook.com/profile.php?id=100005241454058"></a></li>
+                        <li><a href="#" class="fa fa-twitter"></a></li>
+                        <li><a target="_blank" href='https://www.instagram.com/luke_jones8509/' class="fa fa-instagram"></a></li>
+                    </div>
+                </div>
+
+                <!-- <vue-recaptcha sitekey="6LeIP6weAAAAAOshHIwb5EROFETJi-MtbRtdyN2e">
+                <button>Click me</button>
+                </vue-recaptcha> -->
+            </div>
         </div>
     </section>
 </template>
 
 <style>
-#submit-btn{
-    margin-top: 10px;
-    width: 100%;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    border: none;
-    background-color: black;
-    color: white;
+
+
+.contact {
+    display: grid;
+    background-color: white;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas: 'left right';
+    /* -webkit-box-shadow: -33px 23px 2px 2px rgba(0,0,0,1);
+    -moz-box-shadow: -33px 23px 2px 2px rgba(0,0,0,1);
+    box-shadow: -33px 23px 2px 2px rgba(0,0,0,1); */
+}
+
+@media screen and (max-width: 700px){
+    .contact {
+    grid-template-columns: 1fr;
+    grid-template-areas: 
+    'left'
+    'right';
+    }
+
+    .contact-text{
+        justify-content: center;
+        text-align: center;
+    }
 }
 
 #contact-form{
+    grid-area: left;
+    display: grid;
     display: block;
     max-width: 800px;
     margin: 0 auto;
-    border: 2px solid black;
     padding: 30px;
     border-radius: 10px;
-    background-color: gray;
+    background-color: rgb(0, 127, 255);
     margin-bottom: 50px;
+    -webkit-box-shadow: -33px 23px 2px 2px rgba(0,0,0,1);
+    -moz-box-shadow: -33px 23px 2px 2px rgba(0,0,0,1);
+    box-shadow: -33px 23px 2px 2px rgba(0,0,0,1);
 }
 
 #contact-form textarea{
@@ -48,8 +89,66 @@
     padding-top: 5px;
     padding-bottom: 10px;
     border-radius: 5px;
-    border: 2px solid grey;
+    border: 2px solid black;
     font-size: 14px;
     margin-top: 20px;
+}
+
+#submit-btn{
+    margin-top: 10px;
+    width: 20%;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border: none;
+    background-color: black;
+    color: white;
+    float: left;
+    border-radius: 10px;
+}
+
+#submit-btn:hover{
+    border: 2px solid white;
+}
+
+.contact-text{
+    grid-area: right;
+    display: grid;
+    display: block;
+    text-align: left;
+    padding-left: 100px;
+}
+
+.fa {
+    padding: 10px;
+    font-size: 20px;
+    width: 50px;
+    text-decoration: none;
+}
+
+.fa:hover{
+    text-decoration: none;
+    list-style-type: none;
+
+}
+
+.info li {
+    list-style-type: none;
+    font-size: 22px;
+    padding-top: 30px;
+}
+
+.reseaux li {
+    list-style-type: none;
+    font-size: 50px;
+    padding-top: 70px;
+    float: left;
+}
+
+.fa-twitter{
+    padding-left: 20px;
+}
+
+.fa-instagram{
+    padding-left: 40px;
 }
 </style>
